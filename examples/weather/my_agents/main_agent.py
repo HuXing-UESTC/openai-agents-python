4f4fa9ddae311bd926ae20445df3c160ee404ca0
星橙 <miracle.hx@alibaba-inc.com>
2025-03-13 20:40:15 +0800

    feat: update test

--- examples/weather/my_agents/main_agent.py
+++ examples/weather/my_agents/main_agent.py
@@ -5,5 +5,6 @@ from .ding_agent import ding_send_agent
 my_main_agent = Agent(
     name="My Assistant",
     instructions="You are good at distributing tasks and selecting specific assistants to complete tasks based on user questions",
+    model="gpt-4o",
     handoffs=[weather_search_agent, ding_send_agent]
 )
