b6f9ae73188aceb2da41409c6e640aa772f899ef
星橙 <miracle.hx@alibaba-inc.com>
2025-03-13 21:34:55 +0800

    feat: update test

--- examples/weather/weather_manager.py
+++ examples/weather/weather_manager.py
@@ -5,6 +5,7 @@ import logging
 
 from agents import Runner, custom_span, gen_trace_id, trace
 from my_agents.main_agent import my_main_agent
+from my_agents.weather_agent import weather_search_agent
 
 class WeatherSearch:
     def __init__(self, logger: logging.Logger = None):
@@ -14,5 +15,5 @@ class WeatherSearch:
         trace_id = gen_trace_id()
         print(f"\n\n traceId = {trace_id}")
         with trace("Research trace", trace_id=trace_id):
-            result = await Runner.run(my_main_agent, query)
+            result = await Runner.run(weather_search_agent, query)
             print(f"\n\nresult = {result}")
